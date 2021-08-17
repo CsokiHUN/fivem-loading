5e3f7ef1de13765b5037897dd4be0502864d94f1
Csoki <36936216+CsokiHUN@users.noreply.github.com>
2021-08-17 21:53:54 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,3 @@
--- This resource is part of the default Cfx.re asset pack (cfx-server-data)
--- Altering or recreating for local use only is strongly discouraged.
-
 fx_version("cerulean")
 game("gta5")
 author("Csoki")
