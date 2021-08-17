ac275142fc9db938ff432991b191972b2cf793c4
Csoki <36936216+CsokiHUN@users.noreply.github.com>
2021-08-17 21:53:09 +0200

    first version

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+-- This resource is part of the default Cfx.re asset pack (cfx-server-data)
+-- Altering or recreating for local use only is strongly discouraged.
+
+fx_version("cerulean")
+game("gta5")
+author("Csoki")
+
+files({
+	"index.html",
+	"style.css",
+	"assets/*",
+})
+
+loadscreen("index.html")
